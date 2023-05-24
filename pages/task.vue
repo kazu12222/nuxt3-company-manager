74260f86736d8522cb4cd6c30b00b1ae05d0fefe
kazu12222 <nokaha1222@gmail.com>
2023-05-24 12:32:48 +0900

    base作成

--- pages/task.vue
+++ pages/task.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>A</div>
+  <div class="flex flex-col min-h-screen mx-4">
+    <h3 class="text-2xl font-bold mb-4 text-center">タスク管理</h3>
+    <!-- <div class="w-1/4">
+      <AddCard />
+    </div>
+
+    <div class="flex flex-grow">
+      <List :list="list1" class="flex-grow border-r border-gray-300" />
+      <List :list="list2" class="flex-grow border-r border-gray-300" />
+      <List :list="list3" class="flex-grow" />
+    </div> -->
+  </div>
 </template>
+
+<script setup lang="ts">
+import { TodoListType } from '~/types/types';
+const { todo, doing, done } = taskCard();
+const list1 = <TodoListType>{
+  name: 'TODO',
+  cards: todo,
+  status: 'todo',
+};
+
+const list2 = <TodoListType>{
+  name: 'DOING',
+  cards: doing,
+  status: 'done',
+};
+
+const list3 = <TodoListType>{
+  name: 'DONE',
+  cards: done,
+  status: 'done',
+};
+</script>
